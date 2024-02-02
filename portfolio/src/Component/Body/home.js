83e6d0d5b0bc96b83d1e201771e5655ee517caf0
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-02-02 14:39:22 -0500

    feat- Home section Implementation
    implemented Home section added the contents to it and have updated the pictures

--- portfolio/src/Component/Body/home.js
+++ portfolio/src/Component/Body/home.js
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import profileImage from '../../Images/profilepic.jpeg'
+import HeaderBanner from './pageheading';
 function Home() {
   return (
-    <div>home</div>
-  )
+
+    <div className='flex flex-col justify-center items-center font-mono    '>
+      {/* <HeaderBanner /> */}
+      <div className='flex flex-row items-center'>
+        <img
+          src={profileImage}
+          alt='Profile'
+          className='w-40 h-40 rounded-full mr-4' // Adjust the size and styling as needed
+        />
+        <div>
+        <h3 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+Karthik Vanabhojana        </h3>
+<h4 className="text-2xl leading-7 text-gray-900 sm:truncate sm:text-l sm:tracking-tight">
+          Student at Northeastern University
+        </h4>        </div>
+      </div>
+
+      <div className='text-left mt-10 mr-40 mb-40 ml-40 border-solid border-2 border-sky-500 p-10 bg-slate-400'>
+        <p className='text-lg text-slate-800 pb-5' >
+          Hey everyone! I'm Karthik Vanabhojana, and I am pursuing my Master's in Information Systems at Northeastern Univeristy Boston. 🎓💻 I'm all about using tech to solve real-life puzzles.
+        </p>
+        
+        <p className='text-lg text-slate-800 pb-5'>
+          I'm into Linux, shell scripting, and Golang, and I'm always up for learning and teaming up on cool software projects. I love the fast-paced vibe of tech and can communicate techy stuff in plain English too. 🚀
+        </p>
+
+        <p className='text-lg text-slate-800 pb-5'>
+          Oh, and I've played around with AWS, Kubernetes, Jenkins, and git – you know, the usual tech party. Plus, I've dabbled in open source, 'cause sharing is caring, right? 🌐
+        </p>
+
+        <p className='text-lg text-slate-800 pb-5'>
+          If you're looking for someone who's not just about the code but also about the community, hit me up! Let's chat about how I can bring my tech chops and enthusiasm to your team. #TechLife #MasterOfInfoSys #LetsCollaborate
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home
--- portfolio/src/Component/header/dashboardheader.js
+++ portfolio/src/Component/header/dashboardheader.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Dashboardheader() {
   return (
-      <div className='bg-gray-800 py-4'>
+      <div className='flex flex-nowrap'>
   <ul className='flex space-x-4 justify-center items-center'>
     <li>
-      <a className='text-white hover:text-gray-300' href="/karthik">Karthik</a>
+      <a className='text-red hover:text-gray-300' href="/karthik">Karthik</a>
     </li>
     <li>
-      <a className='text-white hover:text-gray-300' href="/projects">Projects</a>
+      <a className='text-red hover:text-gray-300' href="/projects">Projects</a>
     </li>
     <li>
-      <a className='text-white hover:text-gray-300' href="/blogs">Blogs</a>
+      <a className='text-red hover:text-gray-300' href="/blogs">Blogs</a>
     </li>
     <li>
-      <a className='text-white hover:text-gray-300' href="/resume">Resume</a>
+      <a className='text-red hover:text-gray-300' href="/resume">Resume</a>
     </li>
   </ul>
 </div>
