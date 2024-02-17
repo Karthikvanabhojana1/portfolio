adcd594970e388ccc7bcb5b2358a381233013866
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-02-16 21:44:27 -0500

    commit

--- src/Component/Body/downloadresume.js
+++ src/Component/Body/downloadresume.js
@@ -8,7 +8,7 @@ import {
 
 function Downloadresume() {
     const handleDownload = () => {
-        const url = "/ImageandVids/KarthikVanabhojana.pdf";
+        const url = "https://drive.google.com/file/d/1-shMEHQ2exjTC5Ld-yvsUI6_79N8KYYs/view";
         fetch(url)
           .then((response) => response.blob())
           .then((blob) => {
@@ -28,9 +28,9 @@ function Downloadresume() {
       <button
         type="button"
         className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        onClick={handleDownload}>
+        >
         <LinkIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-        Download Resume
+        <a href="https://drive.google.com/file/d/1-shMEHQ2exjTC5Ld-yvsUI6_79N8KYYs/view" target="_blank" >Download Resume</a>
       </button>
     </span>
   </div>
--- src/Component/header/dashboardheader.js
+++ src/Component/header/dashboardheader.js
@@ -3,20 +3,7 @@ import { FaBars } from 'react-icons/fa';
 
 function Dashboardheader() {
   const [showMenu, setShowMenu] = useState(false);
-  const handleDownload = () => {
-    const url = "https://drive.google.com/file/d/1-shMEHQ2exjTC5Ld-yvsUI6_79N8KYYs/view?usp=drive_link";
-    fetch(url)
-      .then((response) => response.blob())
-      .then((blob) => {
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", "KarthikVanabhojana.pdf");
-        document.body.appendChild(link);
-        link.click();
-        link.parentNode.removeChild(link);
-      });
-  };
+ 
   return (
     <div className='fixed top-0 w-full text-white p-4 z-50'>
       <div className="flex justify-between items-center">
@@ -43,7 +30,7 @@ function Dashboardheader() {
               <a className='text-red hover:text-gray-300 text-2xl' href="#blogs">Blogs</a>
             </li>
             <li>
-              <a className='text-red hover:text-gray-300 text-2xl' href="" onClick={handleDownload}>Resume</a>
+              <a className='text-red hover:text-gray-300 text-2xl' href="https://drive.google.com/file/d/1-shMEHQ2exjTC5Ld-yvsUI6_79N8KYYs/view?usp=drive_link" target="_blank">Resume</a>
             </li>
             <li>
               <a className='text-red hover:text-gray-300 text-2xl' href="https://github.com/Karthikvanabhojana1" target="_blank">Github</a>
